fbc27a5d06e0b1d4354f8cb4c2f6697785d15baa
ermi2021 <78529847+ermi2021@users.noreply.github.com>
2021-03-24 09:34:49 +0300

    Add files via upload

---
+++ REST_FOOD/config/Database.php
@@ -0,0 +1,34 @@
+<?php 
+   class Database {
+       //Development Connection
+    //    private $host = 'localhost';
+    //    private $db_name = 'foodapplication';
+    //    private $username = 'root';
+    //    private $password = '';
+    //    private $conn;
+
+
+       //Production Connection
+       private $host = 'remotemysql.com';
+       private $db_name = 'lqRP5NyKHN';
+       private $username = 'lqRP5NyKHN';
+       private $password = 'je5zFL8DBd';
+       private $conn;
+
+       public function connect() {
+           $this->conn = null;
+
+           try {
+              $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username , $this->password);
+              $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+           
+           }catch(PDOException $e) {
+               echo 'Connection failed:';
+           }
+
+           return $this->conn;
+       }
+
+   }
+
+?>
